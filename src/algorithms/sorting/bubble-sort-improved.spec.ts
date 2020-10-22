efe420a42b6f6d465fdb3a5e177a80d249fb2c16
jeffchong <jeffchong922@outlook.com>
2020-10-22 11:18:25 +0800

    冒泡排序改良算法

---
+++ src/algorithms/sorting/bubble-sort-improved.spec.ts
@@ -0,0 +1,4 @@
+import { bubbleSortImproved } from "./bubble-sort-improved";
+import { testSortAlgorithm } from "./_sort-algorithm-test";
+
+testSortAlgorithm(bubbleSortImproved, 'BubbleSortImproved')
---
+++ src/algorithms/sorting/bubble-sort-improved.ts
@@ -0,0 +1,20 @@
+import { defaultPredicateSwap, swap } from "../../util"
+import { IPredicateSwap } from "./sorting.interface"
+
+export function bubbleSortImproved<T> (array: Array<T>, predicateSwap: IPredicateSwap<T> = defaultPredicateSwap): Array<T> {
+  const copyArray = array.slice()
+  const { length } = copyArray
+
+  // 外循环，表示已经排序过的个数
+  for (let i = 0; i < length; i++) {
+    // 内循环，元素进行冒泡
+    for (let j = 0; j < length - 1 - i; j++) {
+      // 决定元素是否进行交换
+      if (predicateSwap(copyArray[j], copyArray[j + 1])) {
+        swap(copyArray, j, j + 1)
+      }
+    }
+  }
+
+  return copyArray
+}
